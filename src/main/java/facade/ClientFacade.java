4822d5d7dc43e92b4dd1c75d7639ce82225fc2b5
shalom <shalomcron@gmail.com>
2022-06-02 23:01:08 +0300

    fix bugs

--- src/main/java/facade/ClientFacade.java
+++ src/main/java/facade/ClientFacade.java
@@ -2,7 +2,13 @@ package facade;
 
 import dao.company.CompanyDAO;
 import dao.company.CompanyDAOImpl;
+import dao.coupon.CouponDAO;
+import dao.coupon.CouponDAOImpl;
+import dao.customer.CustomerDAO;
+import dao.customer.CustomerDAOImpl;
 
 public abstract class ClientFacade {
-    private final CompanyDAO companyDAO = CompanyDAOImpl.getInstance();
+    private static final CustomerDAO customerDAO = CustomerDAOImpl.getInstance();
+    private static final CompanyDAO companyDAO = CompanyDAOImpl.getInstance();
+    private static final CouponDAO couponDAO = CouponDAOImpl.getInstance();
 }
