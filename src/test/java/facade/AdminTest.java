7b108fea42264f8ce5c1c4dc57afa887726aa94e
shalom <shalomcron@gmail.com>
2022-06-02 23:40:14 +0300

    fix bugs

--- src/test/java/facade/AdminTest.java
+++ src/test/java/facade/AdminTest.java
@@ -6,8 +6,7 @@ import exceptions.LoginException;
 public class AdminTest {
     public static void test() throws LoginException {
         ClientFacade admin = new AdminFacade();
-        if (admin.login("admin@admin.com", "admin")) {
-            System.out.println("* Welcome Admin *");
-        }
+        admin.login("admin@admin.com", "admin");
+        System.out.println("* Welcome Admin *");
     }
 }
