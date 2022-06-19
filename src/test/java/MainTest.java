3eb4c813441f0bd71d9aa766d18947fb04cecb4e
shalom <shalomcron@gmail.com>
2022-06-19 03:43:55 +0300

    fix bugs

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,46 +1,33 @@
-import dao.*;
+import beans.cliens.Company;
+import beans.cliens.Customer;
 import db.DatabaseManager;
-import exceptions.JDBCException;
-import facade.AdminFacadeTest;
-import facade.CompanyFacadeTest;
-import facade.CustomerFacadeTest;
+import facade.clients.AdminFacade;
+import facade.clients.CompanyFacade;
+import facade.clients.CustomerFacade;
+import facade.login.LoginManager;
 
 public class MainTest {
+    private static final LoginManager loginManager = LoginManager.getInstance();
+    private static AdminFacade adminFacade = null;
+    private static CompanyFacade companyFacade1, companyFacade2, companyFacade3;
+    private static CustomerFacade customerFacade1, customerFacade2, customerFacade3;
+
+    private static Company company1 = new Company(1, "company1", "company1@gmail.com", "company1_password");
+    private static Company company2 = new Company(2, "company2", "company2@gmail.com", "company2_password");
+    private static Customer customer1 = new Customer(1,"customer1", "customer1Family", "customer1@gmail.com", "customer1_password");
+    private static Customer customer2 = new Customer(2,"customer2", "customer2Family", "customer2@gmail.com", "customer3_password");
+    private static Customer customer3 = new Customer(3,"customer3", "customer3Family", "customer3@gmail.com", "customer3_password");
+
     public static void main(String[] args) {
         System.out.println("----- Main Tests START -----");
         System.out.println("---------- dropCreateStrategy ---------");
         DatabaseManager.getInstance().dropCreateStrategy();
-//        DAOTests();
-         fasadeTests();
+        adminFacade = adminLogin();
+        System.out.println("----- Main Tests END -----");
     }
 
-    private static void fasadeTests() {
-        System.out.println(" ---------- adminFacadeTest START ---------");
-        AdminFacadeTest.adminTest();
-        System.out.println("---------- adminFacadeTest END ---------");
-        System.out.println(" ---------- companyFacadeTest START ---------");
-        CompanyFacadeTest.companyTest();
-        System.out.println(" ---------- companyFacadeTest END ---------");
-        System.out.println(" ---------- customerFacadeTest START ---------");
-        CustomerFacadeTest.customerTest();
-        System.out.println(" ---------- customerFacadeTest END ---------");
-        System.out.println(" ----- Main Tests END -----");
+    private static AdminFacade adminLogin() {
+
     }
-
-
-    private static void DAOTests() {
-        try {
-            System.out.println("---------- DAOTests START ---------");
-            CompaniesTests.test();
-            CustomersTest.test();
-            CategoriesTest.test();
-            CouponTest.test();
-            CouponsCustomersTest.test();
-            System.out.println("---------- DAOTests END ---------");
-        } catch (JDBCException e) {
-            System.out.println("DAOTests Exception: " + e.getMessage());
-        }
-    }
-
 
 }
